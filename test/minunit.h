662d6e62f24062985f8c8a2344247635ee9d3904
Luke Bayes <lukebayes@gmail.com>
2021-02-03 09:11:51 -0500

    Fixed linker error for multiple definitions
    The tests_run (previously testsRun) variable was not declared as static
    and this caused a recently updated linker my Ubuntu 20.10 workstation to
    start throwing up.

--- test/minunit.h
+++ test/minunit.h
@@ -9,7 +9,7 @@
 #define muAssert(test, message) if (!(test)) {\
     log_err(message); return message; }
 
-#define muRunTest(test) message = test(); testsRun++; if (message) return message;
+#define muRunTest(test) message = test(); tests_run++; if (message) return message;
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
     printf("\n----\nRUNNING: %s\n", argv[0]);\
@@ -20,10 +20,10 @@
     else {\
         printf("\033[1m\x1b[32mALL TESTS PASSED\x1b[00m\033[0m\n");\
     }\
-    printf("Tests run: %d\n", testsRun);\
+    printf("Tests run: %d\n", tests_run);\
     exit(result != 0);\
 }
 
-int testsRun;
+static int tests_run;
 
 #endif // _minunit_h
