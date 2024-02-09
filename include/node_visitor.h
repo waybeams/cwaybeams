2ff5fac516b05610ad1658ffae1554f0ff2df9c0
Luke Bayes <lukebayes@gmail.com>
2024-02-08 20:40:08 -0500

    Minor cleanup

--- include/node_visitor.h
+++ include/node_visitor.h
@@ -10,17 +10,41 @@ typedef enum {
 	VISIT_MATCHED,
 } visit_status_t;
 
-typedef visit_status_t (*visit_handler_t)(node_t *node, void *userdata);
+typedef visit_status_t (*visit_handler_t)(node_t *node, void *payload);
 
+/**
+ * Call visitHandler for each child in the provided node.
+ *
+ * Does not recurse into child elements.
+ */
 visit_status_t each_child(node_t *node, visit_handler_t visit_handler,
-                          void *userdata);
-visit_status_t depth_first(node_t *node, visit_handler_t handler,
-                           void *userdata);
-visit_status_t breadth_first(node_t *node, visit_handler_t handler,
-                             void *userdata);
+    void *payload);
 
+/**
+ * Call visitHandler for every node that is underneath the provided Node in
+ * a depth first traversal.
+ *
+ * This is generally useful when searching for values that are known to exist on
+ * leaf nodes.
+ */
+visit_status_t depth_first(node_t *node, visit_handler_t handler,
+    void *payload);
+
+/**
+ * Call visitHandler for every node that is underneath the provided Node in
+ * a breadth first traversal.
+ *
+ * This is generally what people expect when searching the tree for elements
+ * with a given attribute value.
+ */
+visit_status_t breadth_first(node_t *node, visit_handler_t handler,
+    void *payload);
+
+/**
+ * Find the first node that has an attribute with the given type and value.
+ */
 node_t *find_element_with_matching_char_attr(node_t *node, attr_type_t type,
-                                             char *value, void *userdata);
+    char *value, void *payload);
 
 #endif // __node_visitor_h__
 
--- src/node_visitor.c
+++ src/node_visitor.c
@@ -3,17 +3,12 @@
 #include <stddef.h>
 #include <string.h>
 
-/**
- * Call visitHandler for each child in the provided node.
- *
- * Does not recurse into child elements.
- */
 visit_status_t each_child(node_t *node, visit_handler_t visit_handler,
-                          void *userdata) {
+    void *payload) {
   node_t **kids = get_children(node);
   for (u32_t i = 0; i < node->child_count; i++) {
     node_t *kid = kids[i];
-    visit_status_t status = visit_handler(kid, userdata);
+    visit_status_t status = visit_handler(kid, payload);
     if (status != VISIT_SUCCESS) {
       return status;
     }
@@ -21,48 +16,34 @@ visit_status_t each_child(node_t *node, visit_handler_t visit_handler,
   return VISIT_SUCCESS;
 }
 
-/**
- * Call visitHandler for every node that is underneath the provided Node in
- * a depth first traversal.
- *
- * This is generally useful when searching for values that are known to exist on
- * leaf nodes.
- */
 visit_status_t depth_first(node_t *node, visit_handler_t visitHandler,
-                           void *userdata) {
+    void *payload) {
   visit_status_t status;
   node_t **kids = get_children(node);
   for (u32_t i = 0; i < node->child_count; i++) {
-    status = depth_first(kids[i], visitHandler, userdata);
+    status = depth_first(kids[i], visitHandler, payload);
     if (status != VISIT_SUCCESS) {
       return status;
     }
   }
 
-  status = visitHandler(node, userdata);
+  status = visitHandler(node, payload);
   if (status != VISIT_SUCCESS) {
     return status;
   }
   return VISIT_SUCCESS;
 }
 
-/**
- * Call visitHandler for every node that is underneath the provided Node in
- * a breadth first traversal.
- *
- * This is generally what people expect when searching the tree for elements
- * with a given attribute value.
- */
 visit_status_t breadth_first(node_t *node, visit_handler_t visitHandler,
-                             void *userdata) {
-  visit_status_t status = visitHandler(node, userdata);
+    void *payload) {
+  visit_status_t status = visitHandler(node, payload);
   if (status != VISIT_SUCCESS) {
     return status;
   }
 
   node_t **kids = get_children(node);
   for (u32_t i = 0; i < node->child_count; i++) {
-    status = breadth_first(kids[i], visitHandler, userdata);
+    status = breadth_first(kids[i], visitHandler, payload);
     if (status != VISIT_SUCCESS) {
       return status;
     }
@@ -77,7 +58,7 @@ static attr_type_t matching_type;
 static node_t *matched_node;
 
 static visit_status_t matching_char_visit_handler(node_t *node,
-                                                  UNUSED void *userdata) {
+    UNUSED void *payload) {
   char *data = get_char_attr_from_node(node, matching_type, "");
   if (strcmp(data, matching_value) == 0) {
     matched_node = node;
@@ -88,12 +69,12 @@ static visit_status_t matching_char_visit_handler(node_t *node,
 }
 
 node_t *find_element_with_matching_char_attr(node_t *node, attr_type_t type,
-                                             char *value, void *userdata) {
+    char *value, void *payload) {
   matched_node = NULL;
   matching_type = type;
   matching_value = value;
   visit_status_t status = breadth_first(node, matching_char_visit_handler,
-                                        userdata);
+                                        payload);
 
   if (status == VISIT_MATCHED) {
     return matched_node;
